0ee21f90bdd39c4d0f8e6e0fa480f88ce489eda5
qcts33 <qcthreestones@gmail.com>
2020-04-06 09:05:56 +0800

    using var

--- bupt_proxy.user.js
+++ bupt_proxy.user.js
@@ -8,8 +8,8 @@
 // ==/UserScript==
 
 function BuptProxyUrl(href) {
-    bupt_url = 'libcon.bupt.edu.cn';
-    bupt_path = '/https/77726476706e69737468656265737421f9f244993f20645f6c0dc7a59d50267b1ab4a9';
+    var bupt_url = 'libcon.bupt.edu.cn';
+    var bupt_path = '/https/77726476706e69737468656265737421f9f244993f20645f6c0dc7a59d50267b1ab4a9';
     var url = new URL(href);
     url.hostname = bupt_url;
     url.pathname = bupt_path + url.pathname;
